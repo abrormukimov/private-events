05ccaeea10742da4d50785e84deb38ea369b2202
abrormukimov <abrormm34@gmail.com>
2020-07-29 13:33:22 +0500

    Add integration tests for management features

--- spec/model/event_spec.rb
+++ spec/model/event_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
+  before(:each) do
+    User.create(name: 'Abror')
+    Event.create(title: 'title', description: 'description', date: Time.now, creator_id: 1)
+  end
+
   subject do
     User.create(name: 'Abror')
     Event.new(title: 'title',
@@ -42,5 +47,19 @@ RSpec.describe Event, type: :model do
     it { expect(subject).to have_many(:attendees) }
 
     it { expect(subject).to have_many(:attendances) }
+  end
+
+  describe 'past and upcomings events' do
+    it 'checks previous events' do
+      Event.create(title: 'title2', description: 'description2', date: Time.now - 1.day, creator_id: 1)
+      past_event = Event.past.any? { |event| event.date < Time.now }
+      expect(past_event).to eq(true)
+    end
+
+    it 'checks upcoming events' do
+      Event.create(title: 'title3', description: 'description3', date: Time.now + 1.day, creator_id: 1)
+      upcoming_event = Event.upcoming.any? { |event| event.date >= Time.now }
+      expect(upcoming_event).to eq(true)
+    end
   end
 end
